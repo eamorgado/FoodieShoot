6a85f199e346a7852cb2861df34fa5166aee1971
Eduardo Morgado <edu.dado99@gmail.com>
2020-02-15 12:26:53 +0000

    Update urls.py

--- Backend/foodieshoot_api/users/urls.py
+++ Backend/foodieshoot_api/users/urls.py
@@ -3,9 +3,9 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('singup/',views.singup,name='fs-singup'),
-    path('singin/',auth_views.LoginView.as_view(template_name='users/singin.html'),name='fs-singin'),
-    path('singout/',auth_views.LogoutView.as_view(template_name='users/singout.html'),name='fs-singout'),
+    path('signup/',views.signup,name='fs-signup'),
+    path('signin/',auth_views.LoginView.as_view(template_name='users/signin.html'),name='fs-signin'),
+    path('signout/',auth_views.LogoutView.as_view(template_name='users/signout.html'),name='fs-signout'),
     path('profile/',views.profile,name='fs-profile'),
     path('password-reset/',auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),name='password_reset'),
     path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),name='password_reset_done'),
